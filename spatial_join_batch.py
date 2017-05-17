ac43f913c3863a26b0ac96490d86ec24d5f21fe9
dbaharc <dbaharc@gmail.com>
2017-05-17 11:34:59 -0400

    Update spatial_join_batch.py

--- spatial_join_batch.py
+++ spatial_join_batch.py
@@ -17,24 +17,24 @@ except:
     import win32com.client
     gp = win32com.client.Dispatch("esriGeoprocessing.GpDispatch.1")
     print "\nImporting geoprocessor for 9.1 and before..."
-    
-targetFeatures = "W:/SBA/shapefiles/sba_data/"
-joinFeatures = "W:/My Data/Shapefiles/CongressionalDistricts/"
-outfc = "W:/SBA/shapefiles/sba_districts_join/"
-outascii = "W:/SBA/data/"
+
+masterpath = "X:/Raj Dany/geocoding/"
+targetFeatures = masterpath + "intermediate/"
+joinFeature = masterpath + "airportsData/XYAirportsWCountryCodes.shp"
+output = masterpath + "output/"
 
 # Read all the shapefiles in folder 'targetFeatures'
 for shp in os.listdir(targetFeatures):
-    if shp.endswith('shp'):        
+    if shp.startswith('XY'):        
         # Split on the extension
         base = shp.split(".")
         # Get the numbers
-        code = base[0][-3:]
-        
+        code = base[2][-3:]
+        print(code)
+        outfc = output + code + "Airports.sph
+        print(outfc)
         # Do it
-        print "Joining "+targetFeatures+shp+" with "+joinFeatures+"districts"+code+".shp to get "+outfc+shp
-        gp.SpatialJoin(targetFeatures+shp, joinFeatures+"districts"+code+".shp", outfc+shp, "JOIN_ONE_TO_ONE", "KEEP_ALL")		
-
-#arcpy.ExportXYv_stats("//Mac/Home/Documents/ArcGIS/Default.gdb/XYPubGeoCoord_SpatialJoin","gc_pairid;lat;lon;airportid;airportNam;airportCod;airportC_1;airportISO;airportI_1","COMMA","X:/Raj Dany/geocoding/PubGeoCoordAirports","ADD_FIELD_NAMES")
-
+        print "Joining "+targetFeatures+shp+" with "+joinFeature + "to get "+ outfc
+        gp.SpatialJoin(targetFeatures+shp, joinFeature, outfc, "JOIN_ONE_TO_MANY", "KEEP_ALL","WITHIN_A_DISTANCE","50 Miles","#")
+        gp.ExportXYv_stats(outfc,"gc_pairid;lat;lon;airportid;airportNam;airportCod;airportC_1;airportISO;airportI_1","COMMA",output + base + "Airports.csv","ADD_FIELD_NAMES")
 print "\nDone."
